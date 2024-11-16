f610ceb872bf2dfcaa71ab8fb327bec0c6d9e681
Bravin Murambi <bravvjr@gmail.com>
2024-11-16 03:10:05 +0300

    Added my project

--- src/Team.jsx
+++ src/Team.jsx
@@ -17,7 +17,7 @@ function Team() {
       <div className="col-md-3" data-aos="fade-down" data-aos-delay="150">
         <div className="team-member image-zoom">
           <div className="image-zoom-wrapper">
-            <img className='rounded-circle' src="./src/assets/WhatsApp Image 2024-11-13 at 4.22.28 PM (2).jpeg" alt="" />
+            <img className='rounded-circle' src="./images/WhatsApp Image 2024-11-13 at 4.22.28 PM (2).jpeg" alt="" />
           </div>
           <h5 className="mt-3">Daniel Mburu</h5>
           <a href="https://github.com/waweru89">Github</a>
@@ -26,7 +26,7 @@ function Team() {
       <div className="col-md-3" data-aos="fade-down" data-aos-delay="250">
       <div className="team-member image-zoom">
           <div className="image-zoom-wrapper">
-            <img className='rounded-circle' src="./src/assets/jr.jpeg" alt="Betty" />
+            <img className='rounded-circle' src="./images/jr.jpeg" alt="Betty" />
           </div>
           <h5 className="mt-3">Betty Jelagat</h5>
           <a href="https://github.com/bettyje">Github</a>
@@ -35,7 +35,7 @@ function Team() {
       <div className="col-md-3" data-aos="fade-down" data-aos-delay="350">
       <div className="team-member image-zoom">
           <div className="image-zoom-wrapper">
-            <img className='rounded-circle' src="./src/assets/file.jpg" alt="" />
+            <img className='rounded-circle' src="./images/file.jpg" alt="" />
           </div>
           <h5 className="mt-3">Joses Thuo</h5>
           <a href="https://github.com/joses-kt">Github</a>
@@ -44,7 +44,7 @@ function Team() {
       <div className="col-md-3" data-aos="fade-down" data-aos-delay="350">
       <div className="team-member image-zoom">
           <div className="image-zoom-wrapper">
-            <img className='rounded-circle' src="./src/assets/jr.jpg" alt="" />
+            <img className='rounded-circle' src="./images/jr.jpg" alt="" />
           </div>
           <h5 className="mt-3">Bravin Murambi</h5>
           <a href="https://github.com/bravvjr">Github</a>
